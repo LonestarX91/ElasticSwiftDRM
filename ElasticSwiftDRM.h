88d07bfd2bb2143d683282191f35b0c7aa37ea27
LonestarX <me@lonestarx.net>
2023-09-28 01:12:48 +0300

    Initial commit

---
+++ ElasticSwiftDRM.h
@@ -0,0 +1,18 @@
+//
+//  ElasticSwiftDRM.h
+//  ElasticSwiftDRM
+//
+//  Created by Daniel on 25.09.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ElasticSwiftDRM.
+FOUNDATION_EXPORT double ElasticSwiftDRMVersionNumber;
+
+//! Project version string for ElasticSwiftDRM.
+FOUNDATION_EXPORT const unsigned char ElasticSwiftDRMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ElasticSwiftDRM/PublicHeader.h>
+
+
